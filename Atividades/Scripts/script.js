1aa95ca850d11c9f7c1a5de70126e79a03543afc
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2022-05-06 18:14:22 -0300

    Algumas coisas de JS

--- Atividades/Scripts/script.js
+++ Atividades/Scripts/script.js
@@ -1,2 +1,18 @@
 // https://stackoverflow.com/questions/66457318/highlight-text-temporarily
 // https://stackoverflow.com/questions/27656634/button-display-inline-css
+// clicar para esconder uma seção
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
