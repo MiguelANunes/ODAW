dffbdc956f6e1c3867b86d73ba8bba0ee51d2ba4
MiguelANunes <miguel.alfredo.nunes@gmail.com>
2022-07-20 18:51:28 -0300

    Telas Populadas e linkadas

--- Trabalho Final/sitehandler/mainApp/urls.py
+++ Trabalho Final/sitehandler/mainApp/urls.py
@@ -4,9 +4,14 @@ from mainApp import views
 app_name = "Aeroporto"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('aviao/', views.aviao, name='aviao'),
-    path('find/', views.find, name='find')
+    path('',             views.login, name='index'),
+    path('login/',       views.login, name='login'),
+    path('cadastro/',    views.cadastro, name='cadastro'),
+    path('aviao/',       views.aviao, name='aviao'),
+    path('modelo/',      views.modelo, name='modelo'),
+    path('teste/',       views.teste, name='teste'),
+    path('sindicato/',   views.sindicato, name='sindicato'),
+    path('controlador/', views.controlador, name='controlador'),
+    path('tecnico/',     views.tecnico, name='tecnico'),
+    path('find/',        views.find, name='find')
 ]
--- Trabalho Final/sitehandler/mainApp/views.py
+++ Trabalho Final/sitehandler/mainApp/views.py
@@ -10,17 +10,7 @@ from utils import get_db_handle
 # Create your views here.
 
 def index(request):
-    client, aeroporto = get_db_handle()
-
-    avioes_collection = aeroporto['avioes']
-    avioes = avioes_collection.find()
-
-    ret = []
-
-    for aviao in avioes:
-        ret.append(f"coda:{aviao['coda']}; codm:{aviao['codm']}; pontuacao{aviao['pontuacao']}<br>")
-    
-    return HttpResponse(ret)
+    return HttpResponse("Nada para ver aqui")
 
 def login(request):
     return render(request, 'mainApp/login.html')
@@ -29,14 +19,52 @@ def find(request):
     return render(request, 'mainApp/find.html')
 
 def cadastro(request):
-    return render(request, 'mainApp/cadastro.html', {'teste':"Olá, isso é um teste"})
+    return render(request, 'mainApp/cadastro.html')
 
 def aviao(request):
-    client, aeroporto = get_db_handle()
-    # avioes = aeroporto['avioes']
-
+    _, aeroporto = get_db_handle()
     avioes = dbconnection.find(aeroporto, 'avioes', {})
 
     ret = {'avioes': {item['_id']:item for item in avioes}}
 
     return render(request, 'mainApp/tabelas/aviao.html',ret)
+
+def modelo(request):
+    _, aeroporto = get_db_handle()
+    modelos = dbconnection.find(aeroporto, 'modelos', {})
+
+    ret = {'modelos': {item['_id']:item for item in modelos}}
+
+    return render(request, 'mainApp/tabelas/modelo.html',ret)
+
+def teste(request):
+    _, aeroporto = get_db_handle()
+    testes = dbconnection.find(aeroporto, 'testes', {})
+
+    ret = {'testes': {item['_id']:item for item in testes}}
+
+    return render(request, 'mainApp/tabelas/teste.html',ret)
+
+def sindicato(request):
+    _, aeroporto = get_db_handle()
+    sindicatos = dbconnection.find(aeroporto, 'sindicatos', {})
+
+    ret = {'sindicatos': {item['_id']:item for item in sindicatos}}
+
+    return render(request, 'mainApp/tabelas/sindicato.html',ret)
+
+def controlador(request):
+    _, aeroporto = get_db_handle()
+    controladores = dbconnection.find(aeroporto, 'controladores', {})
+
+    ret = {'controladores': {item['_id']:item for item in controladores}}
+
+    return render(request, 'mainApp/tabelas/controlador.html',ret)
+
+def tecnico(request):
+    _, aeroporto = get_db_handle()
+    tecnicos = dbconnection.find(aeroporto, 'tecnicos', {})
+
+    ret = {'tecnicos': {item['_id']:item for item in tecnicos}}
+
+    return render(request, 'mainApp/tabelas/tecnico.html',ret)
